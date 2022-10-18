5d2d10db1f063a4fa550b93a57f9a57e56a2009c
hyosikchoi <huchoi24@naver.com>
2022-10-19 01:11:09 +0900

    Add - databinding

--- app/src/main/java/com/hyosik/android/uistatesample/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/uistatesample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.hyosik.android.uistatesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hyosik.android.uistatesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 }
